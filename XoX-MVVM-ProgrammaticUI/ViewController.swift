77eb21ea7203d597fc1b8aee6fda474324f265e1
Yasin Özmen <yasinozmeen3@gmail.com>
2023-06-02 12:20:26 +0300

    Initial Commit

---
+++ XoX-MVVM-ProgrammaticUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XoX-MVVM-ProgrammaticUI
+//
+//  Created by Kadir Yasin Özmen on 2.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
