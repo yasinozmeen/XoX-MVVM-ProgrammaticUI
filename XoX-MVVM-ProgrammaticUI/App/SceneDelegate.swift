65ea25d82d701522eefff6cac9e613fb6bdb3fee
Yasin Özmen <yasinozmeen3@gmail.com>
2023-06-02 12:29:03 +0300

    folder structure created

---
+++ XoX-MVVM-ProgrammaticUI/App/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  XoX-MVVM-ProgrammaticUI
+//
+//  Created by Kadir Yasin Özmen on 2.06.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else {return}
+        let window = UIWindow(windowScene: windowScene)
+        let navigationController = UINavigationController(rootViewController: ViewController())
+        window.rootViewController = navigationController
+        self.window = window
+        self.window?.makeKeyAndVisible()
+    }
+    
+}
+
