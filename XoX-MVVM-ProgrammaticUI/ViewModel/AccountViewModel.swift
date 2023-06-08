bf3e377a0e37c240cc760385737bb84cdec38027
Yasin Özmen <yasinozmeen3@gmail.com>
2023-06-08 11:29:01 +0300

    i added documentation to AccountViewModel

--- XoX-MVVM-ProgrammaticUI/ViewModel/AccountViewModel.swift
+++ XoX-MVVM-ProgrammaticUI/ViewModel/AccountViewModel.swift
@@ -6,8 +6,10 @@
 //
 
 import Foundation
-import Firebase
+import FirebaseAuth
+import FirebaseFirestore
 
+/// Protocol that facilitates communication between AccountPageVC and AccountViewModel.
 protocol AccountDelegate: AnyObject {
     func signOutButtonTapped()
 }
@@ -20,32 +22,33 @@ final class AccountViewModel {
     public var lastUserMail: String?
     
     // MARK: - Functions
+    /// The function called when the "signOut" button is clicked. After that, the saveUserOffline and restartApplication functions are called
     func singOut(){
         lastUserMail = firebaseAuth.currentUser?.email
         do {
             try firebaseAuth.signOut()
-        } catch let signOutError as NSError {
-            print("Error signing out: %@", signOutError)
+        } catch _ as NSError {
+            #warning("error handling")
         }
         saveUserOfline {
             self.restartApplication()
         }
     }
     
+    /// Taking the email information from the lastUserMail variable, the user is saved offline in Firebase Firestore. This is done using the following function
+    /// - Parameter handler: Once the operation is completed, it can be used to call the new function
     func saveUserOfline(handler:  ()->()){
         if let mail = lastUserMail{
             fireBaseDataBase.collection("User").document(mail).setData([
                 "is online":false
             ]) { err in
-                if let err = err {
-                    print("Error writing document: \(err)")
-                } else {
-                }
+#warning("error handling")
             }
             handler()
         }
     }
     
+    /// A function that sends the user to the loginPageVC screen.
     func restartApplication () {
         let viewController = LoginPageVC()
         let navCtrl = UINavigationController(rootViewController: viewController)
